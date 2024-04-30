3e46a5bf3ffd5b8ee2879e5bb05970a2cbf29f29
Alvin V <alvin.v@ad.infosys.com>
2024-04-30 10:09:59 +0530

    main project commit

---
+++ InfyTell/src/main/java/com/example/infytell/InfyTellApplication.java
@@ -0,0 +1,13 @@
+package com.example.infytell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyTellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyTellApplication.class, args);
+	}
+
+}
